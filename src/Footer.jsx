1549f57e0b1a9a04198ce45533470a12e0a72416
Chukwunenye Onyedikachi Flavian <57820624+DikachiFlavian1@users.noreply.github.com>
2024-09-28 16:36:48 +0100

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const date = new Date();
+  const Year = date.getFullYear();
+  return (
+    <footer>
+      <p> Copyright ©{Year} </p>;{" "}
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1 className="header h1"> Kachi's Note </h1>
+      <p>
+        {" "}
+        <h1>
+          moving forward this is where my thoughts and achievements will be.
+        </h1>{" "}
+      </p>
+    </div>
+  );
+}
+export default Note;
